d516df465a27a9d57ea96fc026a257e24297f15c
flokoe <florian.koehler@corpex.de>
2017-09-27 12:49:50 +0200

    Add convertArray method to convert String array to double array

--- src/frequenzverteilung/Frequenzverteilung.java
+++ src/frequenzverteilung/Frequenzverteilung.java
@@ -12,6 +12,9 @@ public class Frequenzverteilung {
 	// Maybe add read file from command arguments
 	public static String filename = "/home/jupiter/workspace/Frequenzverteilung/SenderList";
 
+	// Init var
+	public static int lnr;
+
 	// method which returns the number of line of the file
 	public static int getLineNumber() {
 
@@ -35,17 +38,33 @@ public class Frequenzverteilung {
 
 	}
 
+	// Method to convert string array to double array
+	public static Double[][] convertArray(String[][] senderDataS) {
+		Double[][] data = new Double[lnr][3];
+		
+		for(int p = 0; p < senderDataS.length; p++) {
+			for(int q = 0; q < senderDataS[p].length; q++) {
+				Double d = Double.parseDouble(senderDataS[p][q]);
+				
+				data[p][q] = d;
+			}
+		}
+		
+		return data;
+	}
+
 	// Main method
 	public static void main(String[] args) {
 
 		// Initialize variables
 		String senderName;
-		int senderNumber = 1;
+		int senderNr;
 		String delimiter = " ";
 		String currentLine = "";
-		int lnr = getLineNumber();
+		lnr = getLineNumber();
 		// Initialize two dimensional string array
-		String[][] senderData = new String[lnr][3];
+		String[][] senderDataS = new String[lnr][3];
+		Double[][] senderData = new Double[lnr][3];
 
 		// Create Buffer
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
@@ -58,24 +77,42 @@ public class Frequenzverteilung {
 				// Get current line split it to array
 				// For length of currentLine array get each element and put it in sender Data array
 				for (int h = 0; h < currentLine.split(delimiter).length; h++) {
-					senderData[nr][h] = currentLine.split(delimiter)[h];
+					senderDataS[nr][h] = currentLine.split(delimiter)[h];
 				}
 				nr++;
 
 			}
 
 			// Just print array data
+//			for (int i = 0; i < senderDataS.length; i++) {
+//				for (int k = 0; k < senderDataS[i].length; k++) {
+//					System.out.println(" " + senderDataS[i][k]);
+//				}
+//				System.out.println("");
+//			}
+			
+			// Call convertArray method
+			senderData = convertArray(senderDataS);
+			
 			for (int i = 0; i < senderData.length; i++) {
-				for (int k = 0; k < senderData[i].length; k++) {
-					System.out.println(" " + senderData[i][k]);
-				}
-				System.out.println("");
+			for (int k = 0; k < senderData[i].length; k++) {
+				System.out.println(" " + senderData[i][k]);
 			}
+			System.out.println("");
+		}
 			
 		// catch exception
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		//Create sender
+//		for(int l = 0; l < senderData.length; l++) {
+//			senderNr = l + 1;
+//			senderName = "S" + senderNr;
+//			
+//			Sender senderName = new Sender(senderName, );
+//		}
 
 	}
 
